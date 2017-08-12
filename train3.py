561f937f24ed5501f44b8f9a19aeaabb486b6c92
Marcin Zadka <gamboox@gmail.com>
2017-08-12 10:19:13 +0200

    Change data augmentation

--- train3.py
+++ train3.py
@@ -23,9 +23,9 @@ validation_samples = 400
 # Data generators & augmentation
 
 datagen = ImageDataGenerator(
+    rescale=1./255,
     rotation_range=20,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
+    zoom_range=0.2,
     horizontal_flip=True)
 
 train_generator = datagen.flow_from_directory(
